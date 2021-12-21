76c54eca7054abb5c379d7dd795612ff6b345b6c
shubham43MP <shubhamdixit19950321@gmail.com>
2021-12-21 15:56:52 +0530

    feat: Added basic banner animation

--- sc-client/src/components/molecules/banners/index.tsx
+++ sc-client/src/components/molecules/banners/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react'
 import { BannerType } from '../../../types/banners'
 import './styles.scss'
@@ -7,14 +9,62 @@ type BannerPropType = {
 }
 
 function Banners({ data = [] }: BannerPropType) {
+  const [ index, setIndex ] = React.useState(0)
+  const timeoutRef = React.useRef<NodeJS.Timeout | null>(null)
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current)
+    }
+  }
+
+  React.useEffect(() => {
+    resetTimeout()
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === data.length - 1 ? 0 : prevIndex + 1
+        ),
+      2000
+    )
+
+    return () => {
+      resetTimeout()
+    }
+  }, [ index, data ])
   return (
-    <div className='banner-image-container'>
-      <img
-        className='banner-image'
-        src="/static/images/offers/offer1.jpg"
-        alt="Independence Day Deal - 25% off on shampoo"
-      />
-    </div>
+    <>
+      <div className="slideshow">
+        <div
+          className="slideshowSlider"
+          style={ { transform: `translate3d(${ -index * 100 }%, 0, 0)` } }
+        >
+          { data.map((bannerdata, index) => (
+            <div
+              className="slide"
+              key={ index }
+            >
+              <div className='banner-image-container'>
+                <img
+                  className='banner-image'
+                  src={ bannerdata.bannerImageUrl }
+                  alt={ bannerdata.bannerImageAlt }
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="slideshowDots">
+          {data.map((_, idx) => (
+            <div
+              key={ _.id }
+              className={ `slideshowDot${ index === idx ? ' active' : '' }` }
+              onClick={ () => setIndex(idx) }
+            ></div>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 

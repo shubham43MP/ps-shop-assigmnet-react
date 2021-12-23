57a4bbeb36d6139d2dd72db6fb5aba2e87515a77
shubham43MP <shubhamdixit19950321@gmail.com>
2021-12-23 15:25:57 +0530

    chore: Added basic login route

--- sc-client/src/pages/login/index.tsx
+++ sc-client/src/pages/login/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import { classes } from './style'
+import './styles.scss'
 
 function Login() {
   return (
-    <div>
-        LOGIN
+    <div className='login-container'>
+      <Grid container>
+        <Grid item xs={ 6 }>
+          <Typography sx={ classes.loginTypo }>
+                Login
+          </Typography>
+          <Typography sx={ classes.loginMessage }>
+                Get access to your Orders, Wishlist and Recommendation
+          </Typography>
+        </Grid>
+        <Grid item xs={ 6 }>
+                Login Buttons
+        </Grid>
+      </Grid>
     </div>
   )
 }
---
+++ sc-client/src/pages/login/style.ts
@@ -0,0 +1,9 @@
+export const classes = {
+  loginTypo: {
+    fontSize: { xs: '25px', xl: '50px', lg: '50px' },
+    fontWeight: 900
+  },
+  loginMessage: {
+    fontSize: { xs: '12.5px', xl: '25px', lg: '25px' },
+  }
+}

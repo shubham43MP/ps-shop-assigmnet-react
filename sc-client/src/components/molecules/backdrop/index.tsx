fa5062c8221ab1053d9bf3809e092bc3aadfe155
shubham43MP <shubhamdixit19950321@gmail.com>
2022-01-04 17:33:54 +0530

    Added intermediate progress

--- sc-client/src/components/molecules/backdrop/index.tsx
+++ sc-client/src/components/molecules/backdrop/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
 
 interface IBackropComponentProps {
   open: boolean;

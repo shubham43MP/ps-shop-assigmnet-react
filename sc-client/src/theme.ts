e8a711405553d793f85e903dabe7dcda810e4058
shubham43MP <shubhamdixit19950321@gmail.com>
2022-01-06 14:35:24 +0530

    Removed unwanted comments

--- sc-client/src/theme.ts
+++ sc-client/src/theme.ts
@@ -2,22 +2,6 @@ import { createTheme } from '@mui/material/styles'
 import { red } from '@mui/material/colors'
 import constants from './styles/constants.module.scss'
 
-// declare module '@mui/material/styles' {
-//     interface BreakpointOverrides {
-//       xs: true;
-//       sm: true;
-//       md: true;
-//       lg: true;
-//       xl: true;
-//       mobileS: true;
-//       mobileM: true;
-//       mobileL: true;
-//       tablet: true;
-//       laptop: true;
-//       desktop: true;
-//     }
-//   }
-
 const theme = createTheme({
   palette: {
     primary: {
@@ -33,21 +17,6 @@ const theme = createTheme({
   typography: {
     fontFamily: 'Roboto Condensed'
   }
-  // breakpoints: {
-  //   values: {
-  //     xs: 0,
-  //     sm: 425,
-  //     md: 767,
-  //     lg: 1024,
-  //     xl: 1440,
-  //     mobileS: 0,
-  //     mobileM: 375,
-  //     mobileL: 425,
-  //     tablet: 767,
-  //     laptop: 1024,
-  //     desktop: 1440,
-  //   },
-  // }
 })
 
 export default theme

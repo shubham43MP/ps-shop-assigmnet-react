f1259db0021854f5e75b69af430eebfcfcc9c335
shubham43MP <shubhamdixit19950321@gmail.com>
2022-01-06 17:51:29 +0530

    Added tooltip to relevant icons

--- sc-client/src/components/atoms/cart-item/index.tsx
+++ sc-client/src/components/atoms/cart-item/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tooltip from '@mui/material/Tooltip';
 import './style.scss'
 
 type TCartItem = {
@@ -27,30 +28,36 @@ function CartItem({
         <h4 className='base-font-color heading-product'>{ heading }</h4>
 
         <div className="item-quantity-picker">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="delete-cart-icon"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            onClick={ () => handleDeleteCartItem(itemId) }
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={ 2 }
-              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-            />
-          </svg>
-          <button
-            className="item-btn"
-            disabled={ quantity === 0 }
-            onClick={ () => handleIncrementDecrement(-1, itemId) }
-          >
-            -
-          </button>
+          <Tooltip title="Delete cart item">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="delete-cart-icon"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              onClick={ () => handleDeleteCartItem(itemId) }
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={ 2 }
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+          </Tooltip>
+          <Tooltip title="Decrease Quantity">
+            <button
+              className="item-btn"
+              disabled={ quantity === 0 }
+              onClick={ () => handleIncrementDecrement(-1, itemId) }
+            >
+              -
+            </button>
+          </Tooltip>
           <p className="item-unit base-font-color">{ quantity }</p>
-          <button className="item-btn" onClick={ () => handleIncrementDecrement(1, itemId) }>+</button>
+          <Tooltip title="Increase Quantity">
+            <button className="item-btn" onClick={ () => handleIncrementDecrement(1, itemId) }>+</button>
+          </Tooltip>
           <p className="item-unit-price base-font-color">
             <strong className='strong-x-element'>X</strong><span>&#8377;</span>{` ${ unitPrice }`}</p>
         </div>
--- sc-client/src/components/molecules/cart/index.tsx
+++ sc-client/src/components/molecules/cart/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import Tooltip from '@mui/material/Tooltip';
 import { ProductType } from 'types/products'
 import CartItem from 'components/atoms/cart-item'
 import { LOWEST_PRICE_PROMOTION } from 'apis/constants'
@@ -51,17 +52,19 @@ function Cart({ cart = [], handleClose }: TCartProps) {
             {`(${ cart.length } items)`}
           </span>
         </p>
-        <svg xmlns="http://www.w3.org/2000/svg"
-          className="close-icon"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          onClick={ handleClose }
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd" />
-        </svg>
+        <Tooltip title="Close">
+          <svg xmlns="http://www.w3.org/2000/svg"
+            className="close-icon"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            onClick={ handleClose }
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd" />
+          </svg>
+        </Tooltip>
       </div>
       <div className={ `cart-contents ${ isCartEmpty ? 'cart-content-empty' : '' }` }>
         {

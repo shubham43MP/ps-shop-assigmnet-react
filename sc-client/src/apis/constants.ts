7f812dec8164aaf305546fa60789b40c1585e46a
shubham43MP <shubhamdixit19950321@gmail.com>
2022-01-06 15:02:07 +0530

    Replaced server url with the hard coded one

--- sc-client/src/apis/constants.ts
+++ sc-client/src/apis/constants.ts
@@ -1,6 +1,7 @@
 export const BANNER_SLIDESHOW_DELAY = 2200
 export const SABKA_BAZAR_LOGO_PATH = '/static/images/logo.png'
 export const LOWEST_PRICE_PROMOTION = 'static/images/lowest-price.png'
+export const REACT_APP_SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL
 export enum CATEGORY_IDENTIFIER {
   fruit_veg = '5b6899953d1a866534f516e2',
   bakery_dairy = '5b6899123d1a866534f516de',
--- sc-client/src/apis/urls.ts
+++ sc-client/src/apis/urls.ts
@@ -1,4 +1,6 @@
-export const SHOPPING_CATEGORIES = 'http://localhost:5000/categories'
-export const SHOPPING_BANNERS = 'http://localhost:5000/banners'
-export const SHOPPING_PRODUCTS = 'http://localhost:5000/products'
-export const SHOPPING_ADD_TO_CART_POST = 'http://localhost:5000/addToCart'
+import { REACT_APP_SERVER_BASE_URL } from './constants'
+
+export const SHOPPING_CATEGORIES = `${ REACT_APP_SERVER_BASE_URL }/categories`
+export const SHOPPING_BANNERS = `${ REACT_APP_SERVER_BASE_URL }/banners`
+export const SHOPPING_PRODUCTS = `${ REACT_APP_SERVER_BASE_URL }/products`
+export const SHOPPING_ADD_TO_CART_POST = `${ REACT_APP_SERVER_BASE_URL }/addToCart`

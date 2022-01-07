d9558b69b03b4eacabdd4113db48907daaa2ad68
shubham43MP <shubhamdixit19950321@gmail.com>
2022-01-07 12:01:54 +0530

    Added base route redirect to login

--- sc-client/src/Root.tsx
+++ sc-client/src/Root.tsx
@@ -1,5 +1,5 @@
 import React, { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
@@ -15,6 +15,7 @@ const Root = () => (
     <ThemeProvider theme={ theme }>
       <CssBaseline />
       <Routes>
+        <Route path='/' element={ <Navigate to='/login' /> } />
         <Route path='/home' element={ <SuspenseWrapper><Homepage /></SuspenseWrapper> } />
         <Route path='/login' element={ <SuspenseWrapper><LoginPage /></SuspenseWrapper> } />
         <Route path='/register' element={ <SuspenseWrapper><RegisterPage /></SuspenseWrapper> } />
